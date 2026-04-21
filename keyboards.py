eb80bc5ee32dfdd61c3339d2f8cfd10ae8ec8a87
BLOXRIKS <steamvasya01@gmail.com>
2026-04-21 17:37:04 +0300

    Create keyboards.py file with button keyboard layouts for the Telegram clicker bot game

---
+++ keyboards.py
@@ -0,0 +1,31 @@
+# Keyboards for Telegram Clicker Bot Game
+
+# Main Menu Keyboard Layout
+main_menu_keyboard = [
+    ["Start Game"],
+    ["Upgrades"],
+    ["Help"],
+    ["Settings"]
+]
+
+# Game Controls Keyboard Layout
+game_controls_keyboard = [
+    ["Tap"],
+    ["Auto Tap"],
+    ["Upgrade"],
+    ["Back"]
+]
+
+# Upgrades Menu Keyboard Layout
+upgrades_menu_keyboard = [
+    ["Speed Upgrade"],
+    ["Power Upgrade"],
+    ["Return to Main Menu"]
+]
+
+# Navigation Buttons Keyboard Layout
+navigation_buttons_keyboard = [
+    ["Previous"],
+    ["Next"],
+    ["Home"]
+]
